a359e71c2328d9859a6d40d66c4adfa7448a1528
ai4dt-Opshell <phenomx9990@gmail.com>
2023-09-06 17:25:10 +0800

    Create：純淨Vite + Vue3 + Typescript 基礎 Include：eslint + Prettierrc Plugin：unplugin-auto-import + unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [
+        vue(),
+        AutoImport({
+            imports: [
+              "vue",
+              "vue-router",
+              "vuex",
+              {
+                "@vueuse/core": [
+                  "useMouse",
+                  ["useFetch", "useMyFetch"]
+                ],
+                axios: [
+                  ["default", "axios"]
+                ],
+                vue: ["PropType", "defineProps", "InjectionKey", "Ref"]
+              }
+            ],
+            dirs: [],
+            dts: "src/types/auto-imports.d.ts",
+            vueTemplate: false,
+            eslintrc: {
+              enabled: false,
+              filepath: "./.eslintrc-auto-import.json",
+              globalsPropValue: true
+            }
+        }),
+        Components({
+            dirs: ['src/components', 'src/views'], // 指定components位置 預設是'src/components'
+            // include: [/\.vue$/],
+            dts: 'src/types/components.d.ts', // .d.ts生成位置
+            extensions: ['vue'],
+        }),
+    ],
+
+    resolve: {
+        alias: { // 設定別名
+            '@': path.resolve(__dirname, 'src') // 根目錄下的 src 資料夾
+        }
+    }
+});
